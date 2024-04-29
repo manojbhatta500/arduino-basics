a3d16f81eb4dba1ec43dab6f5023ccb093a9c783
manojbhatta500 <manojbhatta500@gmail.com>
2024-04-29 17:00:02 +0545

    c1

---
+++ array/array.ino
@@ -0,0 +1,20 @@
+int a[]= {1,2,3,4,5};
+String b[]= {"done","and","dusted"};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print(a[0]);
+
+  Serial.print(a[2]);
+
+  Serial.print(b[0]);
+  // Serial.print(a[0]);
+  // Serial.print(b[0]);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
